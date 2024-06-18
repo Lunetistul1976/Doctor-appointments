c8e97a6efbdcbe0554a765d901c105501862761b
Rareș-Sabin Vlad <rares.vlad@creatopy.com>
2024-06-19 00:14:59 +0300

    feature: add styles

--- doctorAppointments/appointments/forms.py
+++ doctorAppointments/appointments/forms.py
@@ -7,3 +7,15 @@ class BookingForm(ModelForm):
     class Meta:
         model = Booking
         exclude = ('created_at',)
+        labels = {
+            'age': 'Age',
+            'appointmentDateAndTime': 'Appointment Date and Time',
+            'dateOfBirth': 'Date of Birth',
+            'email': 'Email Address',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'medic': 'Medic',
+            'personalNumericCode': 'Personal Numeric Code',
+            'phoneNumber': 'Phone Number',
+            'typeOfService': 'Type of Service',
+        }
